3cbaea2d40833b30c523514e01094022274319e0
satyam_2604 <satyamagrawalv2604@gmail.com>
2025-01-07 15:16:59 +0530

    add

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,13 @@
+import java.util.*;
 class Welcome{
 	public static void main(String [] args){
 		System.out.println("Welcome to java");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(add);
+	}
+	public static int add(int a, int b){
+		return a+b;
 	}
 }
